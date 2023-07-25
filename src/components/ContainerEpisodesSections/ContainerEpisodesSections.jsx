d3f8262db88e721165ea36c0e46647a204c49467
Eliana Rivarola <micaelatorre@outlook.es>
2023-07-25 12:13:04 -0300

    [fix container episodes]

--- src/components/ContainerEpisodesSections/ContainerEpisodesSections.jsx
+++ src/components/ContainerEpisodesSections/ContainerEpisodesSections.jsx
@@ -3,15 +3,18 @@ import { EpisodesSection } from '../EpisodesSection';
 import styles from './containerEpisodes.module.css';
 import { EpisodeContext } from '../../context/EpisodeContext';
 import { ONE_CHARACTER, TWO_CHARACTER } from '../../constants/characters';
+import Message from '../Message/Message';
 
 const ContainerEpisodesSections = () => {
   const { chosenCharacters } = useContext(EpisodeContext);
   const characterOne = chosenCharacters[ONE_CHARACTER];
   const characterTwo = chosenCharacters[TWO_CHARACTER];
+  const characterOneId = characterOne?.characterId;
+  const characterTwoId = characterTwo?.characterId;
   const [sharedCharacters, setSharedCharacters] = useState({});
 
   useEffect(() => {
-    if (!(characterOne?.characterId && characterTwo?.characterId)) return undefined;
+    if (!(characterOneId && characterTwoId) && characterOneId === characterTwoId) return;
     setSharedCharacters(prevState => ({
       ...prevState,
       episodes: [...characterOne?.episodes, ...characterTwo?.episodes],
@@ -23,13 +26,14 @@ const ContainerEpisodesSections = () => {
 
   return (
     <>
-      {characterOne?.characterId && characterTwo?.characterId && (
+      {characterOneId && characterTwoId && characterOneId !== characterTwoId && (
         <div className={styles.episodes_container}>
           <EpisodesSection title={`${ONE_CHARACTER} - Only Episodes`} chosenCharacters={chosenCharacters[ONE_CHARACTER]} />
           <EpisodesSection title={`${ONE_CHARACTER} & ${TWO_CHARACTER} - Shared Episodes`} chosenCharacters={sharedCharacters} isSharedEpisodes />
           <EpisodesSection title={`${TWO_CHARACTER} - Only Episodes`} chosenCharacters={chosenCharacters[TWO_CHARACTER]} />
         </div>
       )}
+      {characterOneId && characterTwoId && characterOneId === characterTwoId && <Message>You must select two different characters !</Message>}
     </>
   );
 };
